3615ee72315dfc49ce3c8f85bbbe3a73f8a5ddf9
yangph <1515924839@qq.com>
2018-12-27 16:01:37 +0800

    修改hello world

--- RequireJS学习指南/hello-world/main.js
+++ RequireJS学习指南/hello-world/main.js
@@ -1,14 +1,14 @@
 require.config({
-    baseUrl:'/',
-    paths:{
-        "a":"lib/a",
-        "b":"lib/b",
-        "c":"others/c"
+    baseUrl: './',
+    paths: {
+        "a": "lib/a",
+        "b": "lib/b",
+        "c": "others/c"
     }
 });
-require(['a','b','c'],function(a,b,c){
-        	console.log("abc");
-		    a.hello();
-		    b.hello();
-		    c.hello();
-		});
+require(['a', 'b', 'c'], function (a, b, c) {
+    console.log("abc");
+    a.hello();
+    b.hello();
+    c.hello();
+});
